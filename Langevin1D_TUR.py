82c79b658e08da3ec7d5c4998cc23d18be624f64
xiangze <xiangze@gmail.com>
2024-04-09 06:47:00 +0900

    debug Langevin calculation and RHS(b,v)

--- Langevin1D_TUR.py
+++ Langevin1D_TUR.py
@@ -3,26 +3,29 @@ import matplotlib.pyplot as plt
 
 # 1次元ランジュバン方程式の微分形
 # 力A,分散sq2beta
-def dlangevin1D(x,A,sq2beta,N=1):
-    return A(x)+sq2beta*np.random.random(N)
+def dlangevin1D(x,A,sq2beta,rng,N=1):
+    return A(x)+sq2beta*rng.standard_normal(N)
 
-def dlangevin1D_sq(x,A,sq2beta,dt,N=1):
-    return A(x)*dt+sq2beta*np.random.random(N)*np.sqrt(dt)
+def dlangevin1D_sq(x,A,sq2beta,dt,rng,N=1):
+    return A(x)*dt+sq2beta*rng.standard_normal(N)*np.sqrt(dt)
+
     
 #時間刻みdt, 時間Tの間の1次元ランジュバン方程式の時間発展
-def langevin1D(A,sq2beta,T,dt=0.1):
+def langevin1D(A,sq2beta,T,dt=0.1,seed=1):
+    rng = np.random.default_rng(seed)
     x=0
     data=[]
     for i in range(int(T)):
-        x=dlangevin1D(x,A,sq2beta)*dt #dt
+        x=x+dlangevin1D(x,A,sq2beta,rng)*dt 
         data.append(x)
     return np.array(data).reshape(-1)
 
-def langevin1D_sq(A,sq2beta,T,dt=0.1,N=1):
+def langevin1D_sq(A,sq2beta,T,dt=0.1,N=1,seed=1):
+    rng = np.random.default_rng(seed)
     x=0
     data=[]
     for i in range(T):
-        x=dlangevin1D_sq(x,A,sq2beta,dt)
+        x=x+dlangevin1D_sq(x,A,sq2beta,dt,rng)
         data.append(x)
     return np.array(data).reshape(-1)
 
@@ -37,7 +40,7 @@ def calc_score(p,dx):
     return (lp[1:]-lp[:-1])/dx
 
 ### entoropy production ###
-def make_entropy_prod(data,A,D,binsize=200):
+def make_entropy_rate(data,A,D,binsize=200):
 #    D=1/2*sq2beta*sq2beta
     p, bin_edges=histogram_p(data,binsize)
     #print(p.sum())
@@ -46,13 +49,21 @@ def make_entropy_prod(data,A,D,binsize=200):
     score=calc_score(p,dx)    
     Ax=[A(x+dx/2) for x in bin_edges[:-2]]    
     s=(Ax-D*score)*(Ax-D*score)/D
-    return (s*p[:-1]).sum()
+    return (s*p[:-1]).sum() #,(s*p[1:]).sum()
 
-def make_entropy_rate(data,A,D,binsize=200):
-    return make_entropy_prod(data,A,D,binsize)/data.shape[0]
+def make_entropy_rate_debug(data,A,D,binsize=200):
+    p, bin_edges=histogram_p(data,binsize)
+    dx=(data.max()-data.min())/binsize
+    score=calc_score(p,dx)    
+    Ax=[A(x+dx/2) for x in bin_edges[:-2]]    
+    s=(Ax-D*score)*(Ax-D*score)/D
+    return (s*p[:-1]).sum() ,(s*p[1:]).sum(),np.array(Ax).sum(),score.sum(),(Ax-D*score).sum()
+
+#def make_entropy_rate(data,A,D,dt,binsize=200):
+#    return make_entropy_prod(data,A,D,binsize)/(data.shape[0]*dt)
 
 #### mean and variance of f #####
-def make_RHS_a(f,data,A,D,dt=0.1,binsize=200):
+def make_RHS_a(f,data,A,D,binsize=200):
     #<f(AP-D∇P)>=∫dx Pf (A-Dscore) =∫dx Pf(A-D∇P/P)=∫dx PfA-fD∇P=∫dx PfA+DP∇f=<fA+D∇f>
     p, bin_edges=histogram_p(data,binsize)
 #   print(p)
@@ -61,14 +72,31 @@ def make_RHS_a(f,data,A,D,dt=0.1,binsize=200):
     fx=[f(x+dx/2) for x in bin_edges[:-2]]
     Ax=[A(x+dx/2) for x in bin_edges[:-2]]
     #<f*F>
-    aveJ=(fx*(Ax-D*score)*p[:-1]).sum()
+    fdx=fx*(Ax-D*score)
+    aveJ=(fdx*p[:-1]).sum()
     #<fDf>
+    #almost same
+    #varJJ=2*(D*p[:-1]*fx*fx).sum()
     varJ=2*np.array([f(x)*f(x)*D for x in data]).mean()
+    return 2*aveJ*aveJ/(varJ),aveJ,varJ #,varJJ
+
+def make_RHS_eq(data,A,D,binsize=200):
+    p, bin_edges=histogram_p(data,binsize)
+    score=calc_score(p,(data.max()-data.min())/binsize)    
+    dx=(data.max()-data.min())/binsize
+    Ax=[A(x+dx/2) for x in bin_edges[:-2]]
+    fx=(Ax-D*score) #(A-D∇)p)/p
+    #<f*F>
+    aveJ=(fx*fx*p[:-1]).sum()
+    #<fDf>
+    #almost same
+    varJ=2*(fx*fx*D*p[:-1]).sum()
+    #2*np.array([fx*fx*D for x in data]).mean()
     return 2*aveJ*aveJ/(varJ),aveJ,varJ
 
-def make_RHS_b(f,df,data,A,D,dt=0.1):
+def make_RHS_b(f,df,data,A,D):
     #<fA+D∇f>
-    aveJ=np.array([A(x)*f(x)+D*df(x) for x in data]).mean()    
+    aveJ=np.array([A(x)*f(x)+D*df(x) for x in data]).sum() #mean()    
     #<fDf>?
     varJ=2*np.array([f(x)*f(x)*D for x in data]).mean()
     return 2*aveJ*aveJ/(varJ),aveJ,varJ
@@ -77,21 +105,80 @@ def make_RHS_v(f,data,dt=0.1,binsize=200):
     p, bin_edges=histogram_p(data,binsize)
     dx=(data.max()-data.min())/binsize
     #f(x)○dx=f(x+x/2)*dx
-    fdx=np.array([f(x+dx/2) for x in bin_edges[:-2]])/dt
+    #fdx=np.array([f(x+dx/2) for x in bin_edges[:-2]])/dt
+    fdx=np.array([(f(x)+f(x+dx))/2 for x in bin_edges[:-2]])/dt #*dx
     aveJ=(fdx*p[:-1]).sum()   
     v2=(fdx*fdx).mean()
 #    print(v2)
     var=(v2-aveJ*aveJ)
-    return (2*aveJ*aveJ)/(var*dt*dt),aveJ,var
+    return (2*aveJ*aveJ)/(var*dt),aveJ,var*dt
 
 def make_RHS_v2(f,data,dt=0.1):
-    #f(x)○dx=f(x+x/2)*dx
-    fdx=np.array([f((data[i]+data[i+1])/2) for i in range(len(data)-1)])/dt
-    aveJ=fdx.mean()   
+    #f(x)○dx=(f(x)+f(x))/2*dx
+    fdx=np.array([ (f(data[i+1])+f(data[i]))/2*(data[i+1]-data[i]) for i in range(len(data)-1)])/dt
+    aveJ=fdx.mean()
     v2=(fdx*fdx).mean()
-#    print(v2)
     var=(v2-aveJ*aveJ)
-    return (2*aveJ*aveJ)/(var*dt*dt),aveJ,var
+    return (2*aveJ*aveJ)/(var*dt),aveJ,var*dt
+
+def tmp(A,sq2beta,T,dt):
+    rng=np.random.default_rng(1)
+    data=langevin1D_sq(A,sq2beta,T,dt,seed=2)
+    #d1=np.array([dlangevin1D_sq(d,A,sq2beta,dt,rng) for d in data])
+    d1=np.array([A(d)*dt+sq2beta*rng.standard_normal(1)*np.sqrt(dt)  for d in data])
+    #d2=np.array([A(d)*dt  for d in data])
+    d2=np.array([A(d)*dt+sq2beta*rng.standard_normal(1)*np.sqrt(dt)  for d in data])
+#    print(d1,d2)
+    print(d1.sum(),d2.sum())
+
+def compare_ave(data,f,df,A,sq2beta,dt,binsize=200):
+    rng=np.random.default_rng(1)
+    D=1/2*sq2beta*sq2beta
+    p, bin_edges=histogram_p(data,binsize)
+    score=calc_score(p,(data.max()-data.min())/binsize)    
+    dx=(data.max()-data.min())/binsize
+    fx=[f(x+dx/2) for x in bin_edges[:-2]]
+    Ax=[A(x+dx/2) for x in bin_edges[:-2]]
+    Ja=fx*(Ax-D*score)    #<f*F>
+    
+    aveJa=(Ja*p[:-1]).sum()
+    Jb=np.array([A(x)*f(x)+D*df(x) for x in data])
+    di=range(len(data)-1)
+
+    fdx=np.array([ (f(data[i+1])+f(data[i]))/2*(data[i+1]-data[i]) for i in di])/dt
+    print(0,fdx.sum(),fdx.mean())
+    _fdx=np.array([ (f(data[i+1])+f(data[i]))/2*dlangevin1D_sq(data[i],A,sq2beta,dt,rng) for i in di])/dt
+    print(1,_fdx.sum(),_fdx.mean())
+    _fdx=np.array([ (f(data[i+1])-f(data[i]))/2*dlangevin1D_sq(data[i],A,sq2beta,dt,rng)+f(data[i])*dlangevin1D_sq(data[i],A,sq2beta,dt,rng) for i in di])/dt
+    print(2,_fdx.sum(),_fdx.mean())
+    _fdx=np.array([ (f(data[i+1])-f(data[i]))/2*dlangevin1D_sq(data[i],A,sq2beta,dt,rng)+f(data[i])*A(data[i])*dt for i in di])/dt
+    print(3,_fdx.sum(),_fdx.mean())
+    _fdx=np.array([ df(data[i])*(data[i+1]-data[i])/2*dlangevin1D_sq(data[i],A,sq2beta,dt,rng)+f(data[i])*A(data[i])*dt for i in di])/dt
+    print(4,_fdx.sum(),_fdx.mean())
+    _fdx=np.array([ df(data[i])/2*dlangevin1D_sq(data[i],A,sq2beta,dt,rng)*dlangevin1D_sq(data[i],A,sq2beta,dt,rng)+f(data[i])*A(data[i])*dt for i in di])/dt
+    print(5,_fdx.sum(),_fdx.mean())
+    _fdx=np.array([ df(data[i])*D*dt+f(data[i])*A(data[i])*dt for i in di])/dt
+    print(6,_fdx.sum(),_fdx.mean())
+    _fdx=np.array([ df(data[i])*D+f(data[i])*A(data[i]) for i in di])
+    print(7,_fdx.sum(),_fdx.mean())
+    _fdx=np.array([ df(x)*D+f(x)*A(x) for x in data])
+    print(8,_fdx.sum(),_fdx.mean())
+    #A(x)*dt+sq2beta*np.random.random(N)*np.sqrt(dt)
+
+    #ave=fdx.sum()/(dt*dt)
+    ave=fdx.mean()
+    #almost same?
+    varJ=2*np.array([f(x)*f(x)*D for x in data]).mean()   
+    varJ2=2*(D*p[:-1]*fx*fx).sum()
+    v2=(fdx*fdx).mean()
+    var=(v2-ave*ave)
+#    return Ja.mean(),Jb.mean(),aveJb,fdx.mean(),fdx.sum(),fdx.sum()/dt,fdx.sum()/(dt*dt)
+    return [aveJa,Jb.mean(),ave], [varJ,varJ2,var*dt],[(2*aveJa*aveJa)/(varJ),(2*ave*ave)/(var*dt)]
+
+
+def compare_langevin_ave(f,df,A,sq2beta,T,dt,binsize):
+    data=langevin1D_sq(A,sq2beta,T,dt)
+    return compare_ave(data,f,df,A,sq2beta,dt,binsize)    
 
 def calc_ratio_fromdata(s:str,data,f,df,A,D,dt,binsize):
     rs={}
@@ -100,9 +187,11 @@ def calc_ratio_fromdata(s:str,data,f,df,A,D,dt,binsize):
     if("v2" in s):
         rs["v2"]=make_RHS_v2(f,data,dt)
     if("a" in s):
-        rs["a"]=make_RHS_a(f,data,A,D,dt,binsize)
+        rs["a"]=make_RHS_a(f,data,A,D,binsize)
+    if("eq" in s):
+        rs["eq"]=make_RHS_eq(data,A,D,binsize)
     if("b" in s):
-        rs["b"]=make_RHS_b(f,df,data,A,D,dt)
+        rs["b"]=make_RHS_b(f,df,data,A,D)
     return rs
 
 def calc_ratio_langevin1D(s:str,f,df,A,sq2beta,T,dt,binsize):
